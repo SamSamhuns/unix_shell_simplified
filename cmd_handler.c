c27943ce8b6f5130137a1e5d7df282ab8fd10d74
SamSamhuns <sthapuskar@yahoo.com>
2019-02-07 00:01:35 +0400

    Added cmd handler .c and .h files

---
+++ cmd_handler.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "cmd_handler.h"
+
+// function to handle exit statements
+void exit_cmd_handler(void){
+        exit(0);
+}
---
+++ cmd_handler.h
@@ -0,0 +1,1 @@
+void exit_cmd_handler(void); /* function to handle cmd exit*/
