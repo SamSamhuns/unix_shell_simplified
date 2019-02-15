03043cd03c589a29a8bcf6bc793678ec10e9d882
SamSamhuns <sthapuskar@yahoo.com>
2019-02-15 21:39:02 +0400

    Added builtin_cmd_hdr and removed builtin_cmd_hdr.c

--- include/builtin_cmd_handler.h
+++ include/builtin_cmd_handler.h
@@ -1,7 +1,6 @@
 #ifndef CMD_HANDLER_H // Making sure the header files are not included twice
 #define CMD_HANDLER_H
-
-#define MAX_CMD_INPUT_BUFFER 100
+#include "common.h"
 
 void exit_cmd_handler(char ** user_input, char ** parsed_input); /* function to handle cmd exit*/
 void pwd_cmd_handler(char **arr); /* function to handle cmd pwd*/
