4d30b6dc4f1134473fb4ae27409bf8755650ae1a
SamSamhuns <sthapuskar@yahoo.com>
2019-02-16 17:39:22 +0400

    Removed unnecessary DEBUG info

--- src/pwd.c
+++ src/pwd.c
@@ -6,8 +6,6 @@
 
 /* function to handle cmd pwd*/
 void pwd_cmd_handler(){
-        if (DEBUG==1)
-          printf("6) %d %d \n", FILENAME_MAX, MAX_CMD_INPUT_BUFFER);
         char pwd_buff[FILENAME_MAX];
         getcwd(pwd_buff, FILENAME_MAX);
         fprintf(stdout, "%s\n",pwd_buff);
