b2edb4384ef31fa9a24c845e851df9c28783d942
SamSamhuns <sthapuskar@yahoo.com>
2019-02-16 17:28:44 +0400

    Removed unnecessary path include

--- src/cd.c
+++ src/cd.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "../include/common.h"
-#include "../include/builtin_cmd_handler.h"
+#include "common.h"
+#include "builtin_cmd_handler.h"
 
 /* function to handle cmd cd*/
 void cd_cmd_handler(char *arr){
--- src/history.c
+++ src/history.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "../include/common.h"
-#include "../include/builtin_cmd_handler.h"
+#include "common.h"
+#include "builtin_cmd_handler.h"
 
 /* function to handle cmd history*/
 void history_cmd_handler(void) {
