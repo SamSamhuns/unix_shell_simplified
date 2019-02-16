6566007b2a1458e24a57ae6768ca254de6497f0d
SamSamhuns <sthapuskar@yahoo.com>
2019-02-16 17:28:13 +0400

    Redefined global macros; added DEBUG marco

--- include/common.h
+++ include/common.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_H // Making sure the header files are not included twice
 #define COMMON_H
 
-#define MAX_CMD_INPUT_BUFFER 100
+#define MAX_CMD_INPUT_BUFFER 500
+#define DEBUG 1
 
 #endif
