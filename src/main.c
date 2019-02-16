78cf40a4c2daa41bb284b498f77a01706b11ada3
SamSamhuns <sthapuskar@yahoo.com>
2019-02-16 17:27:02 +0400

    Parser properly parses input

--- src/main.c
+++ src/main.c
@@ -6,67 +6,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/builtin_cmd_handler.h"
+#include "common.h"
+#include "builtin_cmd_handler.h"
+#define MAX_INPUT_ARR_LEN 50 /* max number of cmd args entered each time*/
 
-void parser(char ** user_input, char **parsed_input);
+/* Parser function */
+int parser(char *user_input, char *parsed_input[], size_t ui_length);
 
-// function to handle the main loop of the cmd prompt
+/* function to handle the main loop of the cmd prompt */
 int main(void){
 
+        char *parsed_input[MAX_INPUT_ARR_LEN]; /* array of char ptrs to hold parsed input*/
         char *user_input = calloc(MAX_CMD_INPUT_BUFFER, sizeof(char));
-        char *parsed_input = calloc(MAX_CMD_INPUT_BUFFER, sizeof(char));
-
         // main while loop for function
         while (1) {
+                int char_arg_len = 0;
                 // Checking if memory is available in the heap
                 if (user_input == NULL) {
                         printf("Out of memory\n" );
                         return 1;
                 }
                 printf(">> ");
-                fgets(user_input, MAX_CMD_INPUT_BUFFER, stdin);
+                fgets(user_input, MAX_CMD_INPUT_BUFFER, stdin); /* Get user input and auto append \n at end */
+                /* Parse the user_input into a array of char ptrs holding user cmd args */
+                char_arg_len = parser(&user_input[0], parsed_input, strlen(user_input));
 
-                // char *token = strtok(user_input, "");
-                //
-                // while (token != NULL)
-                // {
-                //         printf("%s\n", token);
-                //         token = strtok(NULL, "-");
-                // }
-                parser( &user_input, &parsed_input);
-                printf("INPUTS %s %s\n", user_input, parsed_input );
+                // Debug info
+                if (DEBUG==1) {
+                        fprintf(stdout, "3) USER INPUTED %s \nPARSED %s %lu\n", user_input, parsed_input[0], strlen(parsed_input[0]));
+                }
 
-                if (strcmp(parsed_input, "exit") == 0) {
-                        exit_cmd_handler(&user_input, &parsed_input);
-                        exit(0);
+                for (int i = 0; i<char_arg_len; i++) {
+                        printf("%s\n",parsed_input[i] );
                 }
-                else if (strcmp(user_input, "pwd") == 0) {
-                        char *current_dir;
-                        pwd_cmd_handler(&current_dir);
-                        printf("%s\n", current_dir);
-                        free(current_dir);
+
+
+
+                if ((strcmp(parsed_input[0], "exit") == 0) && char_arg_len==1) {
+                        /* the addr of the user_input must be passed to free
+                           the calloced user_input*/
+                        exit_cmd_handler(&user_input);
                 }
-                else if (strcmp(user_input, "cd") == 0) {
-                        char dir_path[MAX_CMD_INPUT_BUFFER];
-                        strcpy(dir_path, user_input);
-                        cd_cmd_handler(dir_path);
+                else if ((strcmp(parsed_input[0], "pwd") == 0) && char_arg_len==1)  {
+                        /* the current*/
+                        pwd_cmd_handler();
                 }
-                else if (strcmp(user_input, "export") == 0) {
+                else if ((strcmp(parsed_input[0], "cd") == 0) && char_arg_len==2)  {
+                        cd_cmd_handler(user_input);
+                }
+                else if ((strcmp(parsed_input[0], "export") == 0) && char_arg_len==1) {
                         export_cmd_handler();
                 }
-                else if (strcmp(user_input, "history") == 0) {
+                else if ((strcmp(parsed_input[0], "history") == 0) && char_arg_len==1)  {
                         history_cmd_handler();
                 }
                 else {
                         printf("Command not recognized\n");
                 }
+
+
         }
         return 0;
 }
 
-void parser(char ** user_input, char **parsed_input) {
-        int user_input_len;
-        user_input_len = strlen(*user_input);
-        strcpy(*parsed_input, *user_input);
+/* parsing function that returns the number of cmd line args entered by user */
+int parser(char *user_input, char *parsed_arr[], size_t ui_length) {
+        /* int to hold umber of cmd line args */
+        int char_arg_len=0;
 
+        if (user_input[ui_length-1] == '\n') {
+                user_input[ui_length-1] = '\0';
+        }
+
+        /* to prevent temp_input char from being destroyed after func exit */
+        static char temp_input[MAX_CMD_INPUT_BUFFER];
+        strcpy(temp_input, user_input);
+
+        int ptr_pos = 0;
+        /* strtok to delim the func with ' ' and save it in parsed_arr */
+        parsed_arr[ptr_pos] = strtok(temp_input, " ");
+        while (parsed_arr[ptr_pos] != NULL)
+        {
+                char_arg_len++;
+                parsed_arr[++ptr_pos] = strtok(NULL, " ");
+        }
+        return char_arg_len;
 }
