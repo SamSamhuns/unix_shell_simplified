dd6137fc75ec7ba4a3dc69040091bf7897014491
SamSamhuns <sthapuskar@yahoo.com>
2019-02-16 17:27:18 +0400

    Removed unnecessary declarations

--- src/pwd.c
+++ src/pwd.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "../include/common.h"
-#include "../include/builtin_cmd_handler.h"
+#include "common.h"
+#include "builtin_cmd_handler.h"
 
 /* function to handle cmd pwd*/
-void pwd_cmd_handler(char **arr){
-        char *buff = calloc(FILENAME_MAX, sizeof (char));
-        getcwd(buff, FILENAME_MAX);
-        *arr = buff;
+void pwd_cmd_handler(){
+        if (DEBUG==1)
+          printf("6) %d %d \n", FILENAME_MAX, MAX_CMD_INPUT_BUFFER);
+        char pwd_buff[FILENAME_MAX];
+        getcwd(pwd_buff, FILENAME_MAX);
+        fprintf(stdout, "%s\n",pwd_buff);
 }
