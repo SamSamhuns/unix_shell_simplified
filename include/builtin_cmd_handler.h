80315c6991f7a17625d37d9e2a8c892a237e6ec5
SamSamhuns <sthapuskar@yahoo.com>
2019-02-16 17:27:58 +0400

    changed def of exit cmd landler

--- include/builtin_cmd_handler.h
+++ include/builtin_cmd_handler.h
@@ -2,8 +2,8 @@
 #define CMD_HANDLER_H
 #include "common.h"
 
-void exit_cmd_handler(char ** user_input, char ** parsed_input); /* function to handle cmd exit*/
-void pwd_cmd_handler(char **arr); /* function to handle cmd pwd*/
+void exit_cmd_handler(char **user_input_ptr); /* function to handle cmd exit*/
+void pwd_cmd_handler(); /* function to handle cmd pwd*/
 void cd_cmd_handler(char *arr); /* function to handle cmd cd*/
 void export_cmd_handler(void); /* function to handle cmd export*/
 void history_cmd_handler(void); /* function to handle cmd history*/
