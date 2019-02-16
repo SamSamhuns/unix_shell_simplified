3b4a3a84490e09177b48fa29c4be52cb33139de5
SamSamhuns <sthapuskar@yahoo.com>
2019-02-16 17:28:30 +0400

    Remove unnecessary free expressions

--- src/exit.c
+++ src/exit.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "../include/common.h"
-#include "../include/builtin_cmd_handler.h"
+#include "common.h"
+#include "builtin_cmd_handler.h"
 
 // function to handle exit statements
-void exit_cmd_handler(char ** user_input, char ** parsed_input){
+void exit_cmd_handler(char **user_input_ptr){
         // free the allocated memory that is passed as an arg to this func
-        free(*user_input);
-        free(*parsed_input);
+        free(*user_input_ptr);
+        exit(0);
 }
