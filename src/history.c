fb0e405e9722f300cbcf9237ebe028214cff6dc4
SamSamhuns <sthapuskar@yahoo.com>
2019-02-18 00:01:53 +0400

    format edit

--- src/history.c
+++ src/history.c
@@ -5,7 +5,7 @@
 #include "builtin_cmd_handler.h"
 
 /* function to handle cmd history*/
-void history_cmd_handler( int number_of_args, struct Node *head ) {
+void history_cmd_handler(int number_of_args, struct Node *head ) {
         /* func to print to stdout the contens of the linked list */
         print_linked_list(number_of_args, head);
 }
