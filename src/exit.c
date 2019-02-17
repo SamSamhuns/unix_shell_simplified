bcdb7dcebe08589928c92c74f8f437c204314fbe
SamSamhuns <sthapuskar@yahoo.com>
2019-02-17 19:21:22 +0400

    Call free_linked_list to free malloced memory

--- src/exit.c
+++ src/exit.c
@@ -5,8 +5,10 @@
 #include "builtin_cmd_handler.h"
 
 // function to handle exit statements
-void exit_cmd_handler(char **user_input_ptr){
+void exit_cmd_handler(char **user_input_ptr, struct Node *head){
         // free the allocated memory that is passed as an arg to this func
         free(*user_input_ptr);
+        /* freeing memory allocated inside the linked lists */
+        free_linked_list(head);
         exit(0);
 }
