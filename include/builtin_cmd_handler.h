0a79e874a808d65b8352ae01ae4eb46f2c807163
SamSamhuns <sthapuskar@yahoo.com>
2019-02-17 16:41:02 +0400

    Updated history_cmd handler

--- include/builtin_cmd_handler.h
+++ include/builtin_cmd_handler.h
@@ -6,5 +6,5 @@ void exit_cmd_handler(char **user_input_ptr); /* function to handle cmd exit*/
 void pwd_cmd_handler(); /* function to handle cmd pwd*/
 void cd_cmd_handler(char *arr); /* function to handle cmd cd*/
 void export_cmd_handler(void); /* function to handle cmd export*/
-void history_cmd_handler(void); /* function to handle cmd history*/
+void history_cmd_handler(struct Node *head); /* function to handle cmd history*/
 #endif
