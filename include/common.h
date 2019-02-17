bcfcb7fc7d485bbfd6ea1521619673b2ddaa9759
SamSamhuns <sthapuskar@yahoo.com>
2019-02-17 16:40:48 +0400

    Added defs for linked list Node

--- include/common.h
+++ include/common.h
@@ -2,6 +2,21 @@
 #define COMMON_H
 
 #define MAX_CMD_INPUT_BUFFER 500
+#define MAX_INPUT_ARR_LEN 50 /* max number of cmd args entered each time*/
+#define MAX_INPUT_KWRD_LEN 200 /* max len of each cmd line arg */
 #define DEBUG 0
+
+typedef struct Node {
+        char content[MAX_INPUT_KWRD_LEN];
+        struct Node *next;
+} Node;
+
+/* func to add node to end of linked list */
+void push( struct Node * head, char cmd[MAX_INPUT_KWRD_LEN]);
+/* function to taverse through linked list*/
+void traverse_linked_list_stream( struct Node *head, FILE *llptr);
+/* function to load history.txt cmds */
+void load_linked_list (struct Node *head);
+
 
 #endif
