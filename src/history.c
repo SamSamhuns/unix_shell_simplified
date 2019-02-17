4abcd7f704c36e9b7cdb215bffa29a3410853ad1
SamSamhuns <sthapuskar@yahoo.com>
2019-02-17 16:40:04 +0400

    Added traverse func for history cmd

--- src/history.c
+++ src/history.c
@@ -5,6 +5,7 @@
 #include "builtin_cmd_handler.h"
 
 /* function to handle cmd history*/
-void history_cmd_handler(void) {
-
+void history_cmd_handler( struct Node *head ) {
+  /* func to print to stdout the contens of the linked list */
+  traverse_linked_list_stream(head, stdout);
 }
