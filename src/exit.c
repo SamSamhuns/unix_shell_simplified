c4724f5f13fdeddc04e9e31a444f2f3c6adeab1e
SamSamhuns <sthapuskar@yahoo.com>
2019-02-19 00:47:01 +0400

    exit_cmd_handler frees both linked lists and closes open file

--- src/exit.c
+++ src/exit.c
@@ -5,10 +5,17 @@
 #include "builtin_cmd_handler.h"
 
 // function to handle exit statements
-void exit_cmd_handler(char **user_input_ptr, struct Node *head){
+void exit_cmd_handler(char **user_input_ptr,
+                      struct Node *history_head, struct Node *export_head, FILE *fptr){
+        /* Save the linked list into the history.txt file */
+        write_linked_list_history(history_head, fptr);
+
         // free the allocated memory that is passed as an arg to this func
         free(*user_input_ptr);
         /* freeing memory allocated inside the linked lists */
-        free_linked_list_history(head);
+        free_linked_list(history_head);
+        free_linked_list(export_head);
+        fclose(fptr); /* free the file pointer */
+
         exit(0);
 }
