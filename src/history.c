41982330875bae00fd8b79e70adb28bd35afa239
SamSamhuns <sthapuskar@yahoo.com>
2019-02-19 01:15:06 +0400

    Added exclaim cmd handler

--- src/history.c
+++ src/history.c
@@ -9,3 +9,21 @@ void history_cmd_handler(int number_of_args, struct Node *head ) {
         /* func to print to stdout the contens of the linked list */
         print_linked_list_history(number_of_args, head);
 }
+
+/* run cmd from history selected by !
+   returns name of command to run again */
+char *exclaim_cmd_handler(struct Node *head, int arg_order_exclamation) {
+        int arg_iterator = 0;
+        struct Node *cur = head;
+        while ( cur != NULL ) {
+                if (arg_iterator == arg_order_exclamation) {
+                        return (cur->content);
+                        break;
+                }
+                cur = cur->next;
+                arg_iterator++;
+        }
+        /* Control should not reach here because of the "(arg_order_exclamation > number_of_args)"
+           check inside the run_command(...) func in main.c */
+        return NULL;
+}
